d4818ceac7cf31b3838bf218d6ae1913d638de3e
DiverCesar <diver.cesar123@gmail.com>
2026-01-22 09:57:33 -0500

    Adding the coding logic into methos of Cruice Control

--- src/main/java/es/upm/grise/profundizacion/cruiseControl/CruiseControl.java
+++ src/main/java/es/upm/grise/profundizacion/cruiseControl/CruiseControl.java
@@ -1,45 +1,63 @@
+//UNIVERSIDAD DE LAS FUERZAS ARMADAS - ESPE
+//César Luis Galarza Villalva
+//Sangolquí, 22th January, 2026
+
 package es.upm.grise.profundizacion.cruiseControl;
 
 public class CruiseControl {
-	
-	@SuppressWarnings("unused")
-	private Speedometer speedometer;
-	private Integer speedSet;
-	private Integer speedLimit;
+    
+    @SuppressWarnings("unused")
+    private Speedometer speedometer;
+    private Integer speedSet;
+    private Integer speedLimit;
 
-	/*
-	 * Constructor
-	 */
-	public CruiseControl(Speedometer speedometer) {}
-	
-	
-	
-	/*
-	 * Method to code / test
-	 */
-	public void setSpeedSet(int speedSet) {}
-	
-	
+    /*
+     * Constructor
+     */
+    public CruiseControl(Speedometer speedometer) {
+        this.speedometer = speedometer;
+        this.speedSet = null;
+        this.speedLimit = null;
+    }
+    
+    
+    
+    /*
+     * Method to code / test
+     */
+    public void setSpeedSet(int speedSet) throws IncorrectSpeedSetException, SpeedSetAboveSpeedLimitException {
+        if (speedSet <= 0) {
+            throw new IncorrectSpeedSetException("SpeedSet must be strictly positive.");
+        }
+        
+        if (this.speedLimit != null && speedSet > this.speedLimit) {
+            throw new SpeedSetAboveSpeedLimitException("SpeedSet cannot exceed the established SpeedLimit.");
+        }
+        
+        this.speedSet = speedSet;
+    }
+    
+    
 
-	/*
-	 * Other setters & getters
-	 */
-	public Integer getSpeedLimit() {
-		
-		return speedLimit;
-		
-	}
+    /*
+     * Other setters & getters
+     */
+    public Integer getSpeedLimit() {
+        
+        return speedLimit;
+        
+    }
 
-	public void setSpeedLimit(Integer speedLimit) {
-		
-		this.speedLimit = speedLimit;
-		
-	}
+    public void setSpeedLimit(Integer speedLimit) {
+        
+        this.speedLimit = speedLimit;
+        
+    }
 
-	public Integer getSpeedSet() {
-		
-		return speedSet;
-		
-	}
+    public Integer getSpeedSet() {
+        
+        return speedSet;
+        
+    }
 
 }
